9569ee1182c9e662190f87a5b963ab9fa3e20179
Faiz Akbar Daud <faizakbar@klaxonsdept.com>
2020-09-22 14:06:37 +0200

    first commit

--- virtualenv/app/models.py
+++ virtualenv/app/models.py
@@ -2,6 +2,7 @@ from app import db, login, ma
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 
 
@@ -13,7 +14,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    birthday =db.Column(db.DATE)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     
     def __repr__(self):
         return '<User {} {}>'.format(self.username,self.email)
@@ -23,6 +25,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 @login.user_loader
 def load_user(id):
