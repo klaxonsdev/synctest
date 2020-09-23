4ffe857da7f459bd47cfb3c6a5a6c2928ffaa2ca
Faiz Akbar Daud <faizakbar@klaxonsdept.com>
2020-09-23 10:25:53 +0200

    upp

--- virtualenv/app/__init__.py
+++ virtualenv/app/__init__.py
@@ -13,4 +13,4 @@ ma = Marshmallow(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
--- virtualenv/app/models.py
+++ virtualenv/app/models.py
@@ -17,6 +17,12 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
     def __repr__(self):
         return '<User {} {}>'.format(self.username,self.email)
 
@@ -30,6 +36,24 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
+    def follow(self, user):
+            if not self.is_following(user):
+                self.followed.append(user)
+
+        def unfollow(self, user):
+            if self.is_following(user):
+                self.followed.remove(user)
+
+        def is_following(self, user):
+            return self.followed.filter(
+                followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id).order_by(
+                    Post.timestamp.desc())
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -42,6 +66,11 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 #Phonebook Class/Model
 class Phonebook(db.Model):
